e0c42c5096e57aeff17ec5f471f10295012cb2c0
Bianca <chsieh17@ucsc.edu>
2021-04-20 15:11:14 +0800

    points breakdown in main.js

--- src/main.js
+++ src/main.js
@@ -12,3 +12,11 @@ let borderPadding = borderUISize / 3;
 
 let keyUP, keyR, keyLEFT, keyRIGHT;
 let keyW, keyA, keyD;
+
+//Mods/Changes for this Assignment
+// [60] Redesign the game's artwork, UI, and sound to change its theme/aesthetic (to something other than sci-fi)
+// [30] Implement a simultaneous two-player mode
+// [10] Replace the UI borders with new artwork
+// [10] Create a new title screen (e.g., new artwork, typography, layout)
+// [5] Add your own (copyright-free) background music to the Play scene
+// [5] Create a new scrolling tile sprite for the background
